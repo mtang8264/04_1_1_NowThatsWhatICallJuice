6ddea28af3cc1570f336c043e4453953f79a4b01
Michael Tang <mtang8264@gmail.com>
2019-03-10 17:35:02 -0400

    Title stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -122,7 +122,7 @@ public class GameManager : MonoBehaviour
     {
         if (overrideline == "")
         {
-            if (theScript.script[currentLine].isQuestion == false)
+            if (theScript.script[currentLine].isQuestion == false || !finishedTyping)
             {
                 if (finishedTyping)
                 {
